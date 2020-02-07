4dffdb86e84b5d7d3f0fc5982036c5a0508d6f51
Poosana Thassanavisut <poosana@Poosanas-MacBook-Pro.local>
2020-02-07 16:42:41 +0700

    done

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -13,8 +13,28 @@ int main()
     cout << "Average = " << B[0];
     cout << "\nS.D. = " << B[1];
     cout << "\nMax = " << B[2];
-    cout << "\nMin = " << B[3];
+    cout << "\nMin = " << B[3]<<endl;
     return 0;
 }
 
 //Write definition of stat() here 
+void stat(const double A[],int N,double B[])
+{
+
+double avg = 0,max =A[0],min = A[0], sum = 0, SD;
+for(int i =0; i<N; i++)
+{
+    avg += A[i];
+    if(A[i] > max) max = A[i];
+    if(A[i] < min) min = A[i];
+    sum += pow(A[i],2);
+}
+avg /= N;
+
+SD = sqrt(sum/N-pow(avg,2));
+B[0] = avg;
+B[1] = SD;
+B[2] = max;
+B[3] = min;
+}
+
